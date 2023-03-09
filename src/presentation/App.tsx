67e1d255bd95956be64f0fe77c59588c518c949a
Leonardo Corbi <leonardocorbi25@hotmail.com>
2023-03-09 12:56:40 -0300

    feat: inteface updade

---
+++ src/presentation/App.tsx
@@ -0,0 +1,203 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>The Benefits of comer abobora</h1>
+        <h2>Why Practicing Yoga Should Be Part of Your Daily Routine</h2>
+      </header>
+      <main>
+        <article>
+          <p>
+            Yoga isnt a <b>practice</b> that has been around for thousands of years and is still
+            incredibly popular today. It involves a combination of physical postures, breathing
+            exercises, and meditation techniques that can help improve both physical and mental{' '}
+            <b>health</b>. In this article, we'll explore the many benefits of practicing yoga and
+            why it should be a part of your daily routine.
+          </p>
+
+          <form action="" method="get">
+            <div className="compound-input">
+              <label htmlFor="text-input">Text Input</label>
+              <input className="input" id="text-input" type="text" value="prova" disabled />
+            </div>
+
+            <div className="compound-input"
+            
+            
+            
+            
+            
+            
+            
+            >
+              <label htmlFor="text-area">Text area</label>
+              <textarea className="input" name="textArea" id="text-area" rows={4}>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              </textarea>
+            </div>
+
+            <div className="compound-input">
+              <label htmlFor="file-input">File Input</label>
+              <input className="input" type="file" name="fileInput" id="file-input" 
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              />
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           </div>
+          </form>
+
+          <h4>Improved Flexibility and Strength</h4>
+          <p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            One of the most obvious benefits of practicing yoga is improved
+            <b>flexibility and strength</b>. Many of the postures require you to stretch and hold
+            your body in various positions, which can help increase your range of motion and overall
+            flexibility. Additionally, some of the more challenging postures require a great deal of
+            strength and can help build
+            <b>muscle tone and improve overall physical fitness</b>.
+          </p>
+          <figure>
+            <img
+              src="https://cdn.shopify.com/s/files/1/0285/1316/products/w0573_1s_Floral-fountain-art-deco-pattern-wallpaper-Curtain-Call_Repeating-Pattern-Sample-2.jpg?v=1623100025"
+              alt=""
+            />
+          </figure>
+          <h4>Reduced Stress and Anxiety</h4>
+          <p>
+            Yoga is also known for its ability to help reduce stress and anxiety. The combination of
+            deep breathing exercises and meditation techniques can help calm the mind and reduce
+            feelings of tension and worry. Regular yoga practice has been shown to lower levels of
+            the stress hormone cortisol, which can lead to improved overall mental health and
+            wellbeing.
+          </p>
+          <h4>Improved Sleep Quality</h4>
+          <p>
+            If you struggle with getting a good night's sleep, yoga may be able to help. Practicing
+            yoga before bed can help calm your mind and relax your body, making it easier to fall
+            asleep and stay asleep throughout the night. Additionally, some yoga postures are
+            specifically designed to help alleviate insomnia and other sleep disorders.
+          </p>
+          <h4>Improved Digestion</h4>
+          <p>
+            Yoga can also have a positive impact on your digestive system. The physical postures and
+            deep breathing exercises can help stimulate the digestive system, which can lead to
+            improved digestion and a reduced risk of digestive issues like constipation and
+            bloating.
+          </p>
+          <h4>Reduced Risk of Injury</h4>
+          <p>
+            Another benefit of practicing yoga is a reduced risk of injury. Many of the postures are
+            low impact and can help improve your overall balance and coordination. Additionally,
+            practicing yoga can help improve your posture and alignment, which can reduce the risk
+            of back pain and other common injuries.
+          </p>
+          <h4>Conclusion</h4>
+          <p>
+            In conclusion, there are many benefits to practicing yoga on a regular basis. From
+            improved flexibility and strength to reduced stress and anxiety, yoga can have a
+            positive impact on both physical and mental health. So why not give it a try? Whether
+            you're a beginner or an experienced yogi, there's always something new to learn and
+            explore in the world of yoga.
+          </p>
+        </article>
+      </main>
+      <footer>
+        <p>
+          © 2023 Simone Cavallini, UI Designer at
+          <a href="https://www.sorint.com/en/">Sorint.lab</a>
+        </p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
